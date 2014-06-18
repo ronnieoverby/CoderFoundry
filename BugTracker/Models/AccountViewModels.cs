48ef19e2ed439afe3755dbdfa1c6a15a4c4ccc06
aj68124 <ajensen@coderfoundry.com>
2014-06-18 11:14:00 -0400

    Initial Commit

---
+++ BugTracker/Models/AccountViewModels.cs
@@ -0,0 +1,288 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Windows;
+
+namespace BugTracker.Models
+{
+
+    //***************** BEGIN Administrator View Models *****************//
+
+    public class AdminViewModel
+    {
+
+    }
+
+    public class RegisterViewModel
+    {
+        /* We are setting up an optional username parameter, if the user wants to enter a username separate 
+         * from his/her email, we'll handle that here. If the user leaves the UserName field blank, the 
+         * user's email address becomes the username. Therefore, the UserName field is NOT required. */
+
+        [Display(Name = "User name")]
+        [RegularExpression(@"^[a-zA-Z0-9]*$")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        // New Fields added to extend Application User class:
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        // Return a pre-poulated instance of AppliationUser:
+        public ApplicationUser GetUser()
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = this.UserName,
+                FirstName = this.FirstName,
+                LastName = this.LastName,
+                Email = this.Email,
+            };
+            return user;
+        }
+    }
+
+    public class EditUserViewModel
+    {
+        public EditUserViewModel() { }
+
+        // Allow Initialization with an instance of ApplicationUser:
+        public EditUserViewModel(ApplicationUser user)
+        {
+            this.UserName = user.UserName;
+            this.FirstName = user.FirstName;
+            this.LastName = user.LastName;
+            this.Email = user.Email;
+            this.Id = user.Id;
+        }
+
+        private string Id { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        /*        [Display(Name = "Assigned Projects")]
+                public List<Project> UserProjectList()
+                {
+                    ApplicationDbContext AppDB = new ApplicationDbContext();
+                    List<ProjectUser> ProjectUserList = AppDB.ProjectUsers.Where(x => x.UserId == Id).ToList();
+                    List<Project> ProjectList = new List<Project>();
+                    foreach (ProjectUser u in ProjectUserList)
+                    {
+                        ProjectList.Add(ProjDB.Projects.Where(y => y.Id == u.ProjectId).First());
+                    }
+                    return ProjectList;
+                }
+        */
+    }
+
+    // added 6/4
+    public class SelectUserRolesViewModel
+    {
+        public SelectUserRolesViewModel()
+        {
+            this.Roles = new List<SelectRoleEditorViewModel>();
+        }
+
+
+        // Enable initialization with an instance of ApplicationUser:
+        public SelectUserRolesViewModel(ApplicationUser user)
+            : this()
+        {
+            this.UserName = user.UserName;
+            this.FirstName = user.FirstName;
+            this.LastName = user.LastName;
+
+            var Db = new ApplicationDbContext();
+
+            // Add all available roles to the list of EditorViewModels:
+            var allRoles = Db.Roles;
+            foreach (var role in allRoles)
+            {
+                // An EditorViewModel will be used by Editor Template:
+                var rvm = new SelectRoleEditorViewModel(role);
+                this.Roles.Add(rvm);
+            }
+
+            // Set the Selected property to true for those roles for 
+            // which the current user is a member:
+            foreach (var userRole in user.Roles)
+            {
+                var checkUserRole = this.Roles.Find(r => r.Id == userRole.RoleId);
+                if (checkUserRole != null)
+                    checkUserRole.Selected = true;
+            }
+        }
+
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<SelectRoleEditorViewModel> Roles { get; set; }
+    }
+
+    // Used to display a single role with a checkbox, within a list structure - added 6/4
+    public class SelectRoleEditorViewModel
+    {
+        public SelectRoleEditorViewModel()
+        {
+        }
+
+        public SelectRoleEditorViewModel(IdentityRole role)
+        {
+            this.RoleName = role.Name;
+            this.Id = role.Id;
+        }
+
+        public bool Selected { get; set; }
+
+        [Required]
+        public string RoleName { get; set; }
+
+        public string Id { get; set; }
+    }
+
+    //***************** END Administrator View Models *****************//
+
+    //***************** BEGIN Developer View Models *****************//
+
+
+    //***************** END Developer View Models *****************//
+
+    public class DevViewModel
+    {
+
+    }
+
+    //***************** BEGIN Submitter View Models *****************//
+
+    public class SubmitViewModel
+    {
+
+    }
+
+    //***************** END Submitter View Models *****************//
+
+    //***************** BEGIN User View Models *****************//
+
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string Action { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User name or Email")]
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [Display(Name = "User name or Email")]
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [Display(Name = "User name or Email")]
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+    //***************** END User View Models *****************//
+
+}
---
+++ BugTracker/Models/IdentityModels.cs
@@ -0,0 +1,141 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System;
+
+namespace BugTracker.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+
+            return userIdentity;
+        }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string GetHighestUserRole()
+        {
+            IdentityManager m = new IdentityManager(this);
+            if (m.IsUserInRole("Administrator"))
+                return "Administrator";
+            else if (m.IsUserInRole("Developer"))
+                return "Developer";
+            else if (m.IsUserInRole("Submitter"))
+                return "Submitter";
+            else // no assigned role
+                return null;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("Default Connection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+    // added for role management 6/4
+    public class IdentityManager
+    {
+        private ApplicationUser user;
+
+        public IdentityManager() { }
+
+        public IdentityManager(ApplicationUser user)
+        {
+            this.user = user;
+        }
+
+        public bool CreateUser(ApplicationUser user, string password)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = um.Create(user, password);
+            return idResult.Succeeded;
+        }
+
+        public bool RoleExists(string name)
+        {
+            var rm = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            return rm.RoleExists(name);
+        }
+
+
+        public bool CreateRole(string name)
+        {
+            var rm = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            var idResult = rm.Create(new IdentityRole(name));
+            return idResult.Succeeded;
+        }
+
+        public bool AddUserToRole(string userId, string roleName)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = um.AddToRole(userId, roleName);
+            return idResult.Succeeded;
+        }
+
+        public bool RemoveUserFromRole(string userId, string roleName)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = um.RemoveFromRole(userId, roleName);
+            return idResult.Succeeded;
+        }
+
+        public bool IsUserInRole(string rolename)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            try
+            {
+                if (um.IsInRole(user.Id, rolename))
+                {
+                    um.Dispose();
+                    return true;
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                // this is thrown every time the system is unable to find the user ID in relation to the specified role (line 114 above)
+                return false;
+            }
+            return false;
+        }
+
+        public void ClearUserRoles(string userId)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var user = um.FindById(userId);
+            var currentRoles = new List<IdentityUserRole>();
+            currentRoles.AddRange(user.Roles);
+            foreach (var role in currentRoles)
+            {
+                um.RemoveFromRole(userId, role.RoleId);
+            }
+        }
+    }
+}
